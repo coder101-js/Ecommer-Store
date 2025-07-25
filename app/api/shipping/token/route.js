032898cc7fc6eced42bce4a41196eee1c00c0b48
='coder101_js <='op422010@gmail.com>
2025-07-25 19:28:04 +0500

    origin fix

--- app/api/shipping/token/route.js
+++ app/api/shipping/token/route.js
@@ -2,17 +2,6 @@ import { NextResponse } from "next/server";
 import generateToken from "@/Utilities/module";
 
 export const GET = async (req) => {
-  const allowedOrigin = process.env.NEXT_PUBLIC_SITE_URL; // example: https://mycoolapp.com
-  const requestOrigin = req.headers.get("origin");
-  console.log(requestOrigin)
-  // 👮‍♂️ Block if origin is not allowed
-  if (requestOrigin !== allowedOrigin) {
-    return NextResponse.json(
-      { error: "Unauthorized: invalid origin 🚫" },
-      { status: 403 }
-    );
-  }
-
   try {
     const token = await generateToken({ id: Date.now() }, "20s");
     return NextResponse.json({ token });
